6925c245ce6806d1e37a44d7db933739fd5513bf
Tom Dakin <tomdakin@Toms-MacBook-Pro.local>
2018-06-29 20:19:09 +0100

    added read_spacy_pickle function and incorporated spacy objects into dataset

--- src/data/dataset.py
+++ src/data/dataset.py
@@ -5,7 +5,9 @@ This module contains the class(es) and functions to read the datasets.
 """
 import csv
 import pandas as pd
-
+import spacy
+import pickle
+from spacy.tokens import Doc
 
 
 class Dataset(object):
@@ -33,23 +35,55 @@ class Dataset(object):
         self._devset = None
         self._testset = None
 
+        """spaCy object handling"""
+        if self._language == "english":
+            self.nlp = spacy.load('en_core_web_lg')
+        elif self._language == "spanish":
+            self.nlp = spacy.load("es_core_news_md")
+        elif self._language == "german":
+            self.nlp = spacy.load('de_core_news_sm')
+        elif self._language == "french":
+            self.nlp = spacy.load('fr_core_news_md')
+
+        self._trainset_spacy_path = "data/interim/{}/{}_Train-spacy-objs.pkl".format(
+            language.lower(), dataset_name)
+        self._devset_spacy_path = "data/interim/{}/{}_Dev-spacy-objs.pkl".format(
+            language.lower(), dataset_name)
+        self._testset_spacy_path = "data/interim/{}/{}_Test-spacy-objs.pkl".format(
+            language.lower(), dataset_name)
+
     def train_set(self):
         """list. Getter method for the training set. """
         if self._trainset is None:  # loads the data to memory once and when requested.
             self._trainset = self.read_dataset(self._trainset_path)
-        return self._trainset
+            self._trainset_spacy = self.read_spacy_pickle(self._trainset_spacy_path)
+
+        if self._trainset_spacy is not None:
+            return pd.concat([self._trainset, self._trainset_spacy], axis=1)
+        else:
+            return self._trainset
 
     def dev_set(self):
         """list. Getter method for the development set. """
         if self._devset is None:  # loads the data to memory once and when requested.
             self._devset = self.read_dataset(self._devset_path)
-        return self._devset
+            self._devset_spacy = self.read_spacy_pickle(self._devset_spacy_path)
+
+        if self._devset_spacy is not None:
+            return pd.concat([self._devset, self._devset_spacy], axis=1)
+        else:
+            return self._devset
 
     def test_set(self):
         """list. Getter method for the test set. """
         if self._testset is None:  # loads the data to memory once and when requested.
             self._testset = self.read_dataset(self._testset_path)
-        return self._testset
+            self._testset_spacy = self.read_spacy_pickle(self._testset_spacy_path)
+
+        if self._testset_spacy is not None:
+            return pd.concat([self._testset, self._testset_spacy], axis=1)
+        else:
+            return self._testset
 
     def read_dataset(self, file_path):
         """Read the dataset file.
@@ -69,3 +103,32 @@ class Dataset(object):
             dataset = pd.read_csv(file, names=fieldnames, sep="\t")
 
         return dataset
+
+    def read_spacy_pickle(self, file_path):
+        """Read the pickled spacy objects
+
+        Args:
+            file_path (str): Path of the pickled spacy objects file
+
+        Returns:
+            pandas DataFrame. A single column of the spacy docs.
+
+        """
+
+        vocab = self.nlp.vocab
+
+        try:
+            file = open(file_path, "rb")
+            # putting the spacy doc in a single-item list to avoid pandas splitting it up
+            spacy_objects = [[Doc(vocab).from_bytes(x)] for x in pickle.load(file)]
+            file.close()
+
+            spacy_objects_dataset = pd.DataFrame(spacy_objects, columns=["spacy"])
+            return spacy_objects_dataset
+
+        except FileNotFoundError:
+            print('spaCy pickle file for {} does not exist. No spaCy objects will be included.'.format(
+                self._dataset_name))
+            return None
+
+        return spacy_objects_dataset
