c3ee94ac73e4bbc033faf47f228487f8a687365f
IntoThePit <theblindorphan@gmail.com>
2018-10-26 11:44:21 +0100

    Nice feature selection for Crosslingual System

--- src/features/affix_features.py
+++ src/features/affix_features.py
@@ -3,13 +3,22 @@
 This module contains functions to detect whether a target word or phrase contains Latin or Greek affixes.
 
 """
-affixes = []
-with open('data/external/greek_and_latin_affixes.txt', encoding='latin-1') as f:
-    for line in f:
-        affixes.append(line.replace("\n", ""))
+from pathlib import Path
+import os
 
+def get_affixes():
+    affixes = []
+    base = Path(os.path.abspath('.'))
+    path_to_affixes = Path('data/external/greek_and_latin_affixes.txt')
+    final_path = base.joinpath(path_to_affixes)
 
-def greek_or_latin(target_word):
+    with open(final_path, encoding='latin-1') as f:
+        for line in f:
+            affixes.append(line.replace("\n", ""))
+    return affixes
+    
+
+def greek_or_latin(target_word, affixes=None):
     """Computes whether the target word contains Greek or Latin affixes.
 
     Args:
@@ -18,4 +27,9 @@ def greek_or_latin(target_word):
     Returns:
         int. 1 if a Greek or Latin affix is contained in the target word, 0 if not.
     """
+    if affixes is None:
+        affixes = get_affixes()
     return int(any(affix in target_word for affix in affixes))
+
+if __name__ == '__main__':
+    affixes = get_affixes()
--- src/features/feature_transfomers.py
+++ src/features/feature_transfomers.py
@@ -235,54 +235,141 @@ class Crosslingual_Feature_Extractor(BaseEstimator, TransformerMixin):
 
     """
 
-    def __init__(self, language=None, ablate=None):
+    def __init__(self, language=None, ablate=None, features_to_use=None):
         """
         Define basic properties
 
         Args:
             language(str): language of input data
+            features_to_use: a list of string named features to use
         """
+        # This dict contains all available features, along with a list of their 
+        # high-computing-power requirements e.g. ['spacy']
+        feature_requirements = {
+                # TODO: Actually fill in these requirements. At the moment, I'm just putting everything as all requirements.
+                'is_nounphrase' : ['spacy'],
+                'len_tokens_norm': ['spacy'],
+                'hypernym_count': None,
+                'len_chars_norm': None,
+                'len_tokens': None,
+                'len_syllables': ['hyph'],
+                'consonant_freq': None,
+                'gr_or_lat': ['affix'],
+                'is_capitalised': None,
+                'num_complex_punct': None,
+                'averaged_chars_per_word': None,
+                'sent_length': None,
+                'unigram_prob': ['unigram_probs'],
+                'char_n_gram_feats': None,
+                'sent_n_gram_feats': None,
+                'iob_tags': ['spacy'],
+                'lemma_feats': ['spacy'],
+                'bag_of_shapes': ['spacy'],
+                'pos_tag_counts': ['spacy'],
+                'NER_tag_counts': ['spacy'],
+                }
+        
+        if features_to_use == None or features_to_use == 'all':
+            features_to_use = list(feature_requirements.keys())
+        
+        # Total requirements is a unique list of all the requirements.
+        self.total_requirements = set()
+        final_features = []
+        for feature in features_to_use:
+            
+            # Making sure that we know about the feature
+            if feature in feature_requirements.keys():
+                if feature_requirements[feature] is not None:
+                    for requirement in feature_requirements[feature]:   
+                        self.total_requirements.add(requirement)
+                final_features.append(feature)
+            else:
+                print("{} did not match any of the features in feature_requirements, so was not used.".format(feature))
+                
+        
+        
+        self.features_to_use = final_features
+        
+        self.affixes = {}
+        self.spacy_models = {'english':None,'spanish':None,'german':None,'french':None}
+        self.hyph_dictionaries = {'english':None,'spanish':None,'german':None,'french':None}
+        self.unigram_prob_dict = {'english':None,'spanish':None,'german':None,'french':None}
+        
+        # So that we're only opening this file once.
+        if 'affix' in self.total_requirements:
+            self.affixes = affix_features.get_affixes()
+        
+        
         if language == 'english':
-            self.spacy_models = {'english': spacy.load('en_core_web_lg')}
-            self.hyph_dictionaries = {'english': pyphen.Pyphen(lang='en')}
-            self.unigram_prob_dict = {'english': file_io.read_file('data/external/english_u_prob.csv')}
+            if 'spacy' in self.total_requirements:
+                self.spacy_models = {'english': spacy.load('en_core_web_lg')}
+            
+            if 'hyph' in self.total_requirements:
+                self.hyph_dictionaries = {'english': pyphen.Pyphen(lang='en')}
+                
+            if 'unigram_probs' in self.total_requirements:
+                self.unigram_prob_dict = {'english': file_io.read_file('data/external/english_u_prob.csv')}
+            
 
         elif language == 'spanish':
-            self.spacy_models = {'spanish': spacy.load('es_core_news_md')}
-            self.hyph_dictionaries = {'spanish': pyphen.Pyphen(lang='es')}
-            self.unigram_prob_dict = {'spanish': file_io.read_file('data/external/spanish_u_prob.csv')}
+            if 'spacy' in self.total_requirements:
+                self.spacy_models = {'spanish': spacy.load('es_core_news_md')}
+                
+            if 'hyph' in self.total_requirements:
+                self.hyph_dictionaries = {'spanish': pyphen.Pyphen(lang='es')}
+                
+            if 'unigram_probs' in self.total_requirements:
+                self.unigram_prob_dict = {'spanish': file_io.read_file('data/external/spanish_u_prob.csv')}
 
         elif language == 'german':
-            self.spacy_models = {'german': spacy.load('de_core_news_sm')}
-            self.hyph_dictionaries = {'german': pyphen.Pyphen(lang='de')}
-            self.unigram_prob_dict = {'german': file_io.read_file('data/external/german_u_prob.csv')}
+            if 'spacy' in self.total_requirements:
+                self.spacy_models = {'german': spacy.load('de_core_news_sm')}
+                
+            if 'hyph' in self.total_requirements:
+                self.hyph_dictionaries = {'german': pyphen.Pyphen(lang='de')}
+                
+            if 'unigram_probs' in self.total_requirements:
+                self.unigram_prob_dict = {'german': file_io.read_file('data/external/german_u_prob.csv')}
 
         elif language == 'french':
-            self.spacy_models = {'french': spacy.load('fr_core_news_md')}
-            self.hyph_dictionaries = {'french': pyphen.Pyphen(lang='fr')}
-            self.unigram_prob_dict = {'french': file_io.read_file('data/external/french_u_prob.csv')}
+            if 'spacy' in self.total_requirements:
+                self.spacy_models = {'french': spacy.load('fr_core_news_md')}
+                
+            if 'hyph' in self.total_requirements:
+                self.hyph_dictionaries = {'french': pyphen.Pyphen(lang='fr')}
+                
+            if 'unigram_probs' in self.total_requirements:    
+                self.unigram_prob_dict = {'french': file_io.read_file('data/external/french_u_prob.csv')}
 
         else:
-            self.spacy_models = {
-                'english': spacy.load('en_core_web_lg'),
-                'spanish': spacy.load("es_core_news_md"),
-                'german': spacy.load('de_core_news_sm'),
-                'french': spacy.load('fr_core_news_md')
-            }
+            if 'spacy' in self.total_requirements:
+                self.spacy_models = {
+                    'english': spacy.load('en_core_web_lg'),
+                    'spanish': spacy.load("es_core_news_md"),
+                    'german': spacy.load('de_core_news_sm'),
+                    'french': spacy.load('fr_core_news_md')
+                }
 
-            self.hyph_dictionaries = {
-                    'english': pyphen.Pyphen(lang='en'),
-                    'spanish': pyphen.Pyphen(lang='es'),
-                    'german': pyphen.Pyphen(lang='de'),
-                    'french': pyphen.Pyphen(lang='fr')
-            }
+            if 'hyph' in self.total_requirements:
+                self.hyph_dictionaries = {
+                        'english': pyphen.Pyphen(lang='en'),
+                        'spanish': pyphen.Pyphen(lang='es'),
+                        'german': pyphen.Pyphen(lang='de'),
+                        'french': pyphen.Pyphen(lang='fr')
+                }
+            
+            if 'unigram_probs' in self.total_requirements:
+                self.unigram_prob_dict = {
+                        'english' : file_io.read_file('data/external/english_u_prob.csv'),
+                        'spanish' : file_io.read_file('data/external/spanish_u_prob.csv'),
+                        'german' : file_io.read_file('data/external/german_u_prob.csv'),
+                        'french': file_io.read_file('data/external/french_u_prob.csv')
+                }
+                
 
-            self.unigram_prob_dict = {
-                    'english' : file_io.read_file('data/external/english_u_prob.csv'),
-                    'spanish' : file_io.read_file('data/external/spanish_u_prob.csv'),
-                    'german' : file_io.read_file('data/external/german_u_prob.csv'),
-                    'french': file_io.read_file('data/external/french_u_prob.csv')
-            }
+                
+            
+                
         self.ablate = ablate
 
     def fit(self, X, *_):
@@ -334,6 +421,34 @@ class Crosslingual_Feature_Extractor(BaseEstimator, TransformerMixin):
         avg_target_phrase_len = np.mean([len(x) for x in X["spacy"]])
 
         """ """
+        
+        # This is a mapping, which allows the system to not run functions that
+        # aren't being used.
+        feature_names_to_funcs = {
+                'is_nounphrase': noun_phrase_features.is_noun_phrase,
+                'len_tokens_norm': length_features.token_length_norm,
+                'hypernym_count': hypernym_features.hypernym_count,
+                'len_chars_norm': length_features.character_length,
+                'len_tokens': length_features.token_length,
+                'len_syllables': phonetic_features.num_syllables,
+                'consonant_freq': phonetic_features.consonant_frequency,
+                'gr_or_lat': affix_features.greek_or_latin,
+                'is_capitalised': morphological_features.is_capitalised,
+                'num_complex_punct': morphological_features.num_complex_punct,
+                'averaged_chars_per_word': length_features.averaged_chars_per_word,
+                'sent_length' : length_features.token_length,
+                'unigram_prob': probability_features.get_unigram_prob,
+                
+                # These are long features, maybe worth considering differently?
+                'char_n_gram_feats': NGram_char_features.getAllCharNGrams,
+                'sent_n_gram_feats': sentence_features.getAllSentNGrams,
+                'iob_tags': iob_features.iob_tags,
+                'lemma_feats': lemma_features.lemmas,
+                'bag_of_shapes': morphological_features.word_shape,
+                'pos_tag_counts': syntactic_features.get_pos_counts,
+                'NER_tag_counts': syntactic_features.get_ne_counts    
+                }
+
         for ix, row in X.iterrows():
 
             """ Selecting """
@@ -341,62 +456,75 @@ class Crosslingual_Feature_Extractor(BaseEstimator, TransformerMixin):
             target_word = row["target_word"]
             target_sent = row["sentence"]
             language = row["language"]
-            spacy_tokens = self.get_spacy_tokens(spacy_sent,
-                                                 target_word,
-                                                 self.spacy_models[language])
-
+            
+            if 'spacy' in self.total_requirements:
+                spacy_tokens = self.get_spacy_tokens(spacy_sent,
+                                                     target_word,
+                                                     self.spacy_models[language])
+            else:
+                spacy_tokens = []
+                
+            func_params = {
+                    'is_nounphrase': (spacy_sent, target_word),
+                    'len_tokens_norm': (spacy_tokens, avg_target_phrase_len),
+                    'hypernym_count': (target_word, language),
+                    'len_chars_norm': (target_word, language),
+                    'len_tokens': (target_word),
+                    'len_syllables': (target_word, language, self.hyph_dictionaries[language]),
+                    'consonant_freq':(target_word),
+                    'gr_or_lat': (target_word, self.affixes),
+                    'is_capitalised': (target_word),
+                    'num_complex_punct': (target_word),
+                    'averaged_chars_per_word': (target_word, language),
+                    'sent_length' : (target_sent),
+                    'unigram_prob': (target_word, language, self.unigram_prob_dict[language]),
+                    
+                    # These are long features, maybe worth considering differently?
+                    'char_n_gram_feats': (target_word, 6),
+                    'sent_n_gram_feats': (target_sent, 3),
+                    'iob_tags': (spacy_tokens),
+                    'lemma_feats': (spacy_tokens),
+                    'bag_of_shapes': (spacy_tokens),
+                    'pos_tag_counts': (spacy_tokens),
+                    'NER_tag_counts': (spacy_tokens)    
+                    }
+                
+            
+            
             """ Feature Extraction """
-            is_nounphrase = noun_phrase_features.is_noun_phrase(spacy_sent, target_word)
-            len_tokens_norm = len(spacy_tokens)/avg_target_phrase_len
-            hypernym_counts = hypernym_features.hypernym_count(target_word, language)
-            len_chars_norm = length_features.character_length(target_word, language=language)
-            len_tokens = length_features.token_length(target_word)
-            len_syllables = phonetic_features.num_syllables(target_word, language, self.hyph_dictionaries[language])
-            consonant_freq = phonetic_features.consonant_frequency(target_word)
-            gr_or_lat = affix_features.greek_or_latin(target_word)
-            is_capitalised = morphological_features.is_capitalised(target_word)
-            num_complex_punct = morphological_features.num_complex_punct(target_word)
-            averaged_chars_per_word = length_features.averaged_chars_per_word(target_word, language)
-            sent_length = length_features.token_length(target_sent)
+            # Long features are those that output whole dictionaries of unknown size
+            long_features = [
+                    'char_n_gram_feats',
+                    'sent_n_gram_feats',
+                    'iob_tags',
+                    'lemma_feats',
+                    'bag_of_shapes',
+                    'pos_tag_counts',
+                    'NER_tag_counts'
+                ]
+            
+            row_dict = {}
+            for feature in self.features_to_use:
+                
+                feature_params = func_params[feature]
+                    
+                if feature in long_features:
+                    if isinstance(feature_params, tuple):
+                        row_result = feature_names_to_funcs[feature](*feature_params).items()
+                    else:
+                        row_result = feature_names_to_funcs[feature](feature_params).items()
+                    row_dict.update(row_result)
+                else:
+                    if isinstance(feature_params, tuple):
+                        row_dict[feature] = feature_names_to_funcs[feature](*feature_params)
+                    else:
+                        row_dict[feature] = feature_names_to_funcs[feature](feature_params)
 
-            row_dict = {
-                'is_nounphrase': is_nounphrase,
-                'len_tokens_norm': len_tokens_norm,
-                'hypernym_count': hypernym_counts,
-                'len_chars_norm': len_chars_norm,
-                'len_tokens': len_tokens,
-                'consonant_freq': consonant_freq,
-                'gr_or_lat': gr_or_lat,
-                'is_capitalised': is_capitalised,
-                'num_complex_punct': num_complex_punct,
-                'averaged_chars_per_word': averaged_chars_per_word,
-                'sent_length' : sent_length,
-                'unigram_prob': probability_features.get_unigram_prob(target_word, language, self.unigram_prob_dict[language])
-
-            }
 
             if self.ablate:
                 for key in self.ablate:
                     if key in row_dict:
                         del row_dict[key]
-
-            long_feat_dict = {
-                'char_n_gram_feats': NGram_char_features.getAllCharNGrams(target_word, 6),
-                'sent_n_gram_feats': sentence_features.getAllSentNGrams(target_sent, 3),
-                'iob_tags': iob_features.iob_tags(spacy_tokens),
-                'lemma_feats': lemma_features.lemmas(spacy_tokens),
-                'bag_of_shapes': morphological_features.word_shape(spacy_tokens),
-                'pos_tag_counts': syntactic_features.get_pos_counts(spacy_tokens),
-                'NER_tag_counts': syntactic_features.get_ne_counts(spacy_tokens)
-            }
-
-            if self.ablate:
-                for key in self.ablate:
-                    if key in long_feat_dict:
-                        del long_feat_dict[key]
-
-            for feat, value_dict in long_feat_dict.items():
-                row_dict.update(value_dict.items())
 
             row_dict = OrderedDict(sorted(row_dict.items(), key=lambda t: t[0]))
 
